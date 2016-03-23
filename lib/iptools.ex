5081057a429b014e670990848f8b58c23f36c072
Kevin Thompson <kevin.david.thompson@gmail.com>
2016-03-23 16:03:24 -0700

    adds is_rfc1918? and to_integer functions

--- lib/iptools.ex
+++ lib/iptools.ex
@@ -1,2 +1,31 @@
 defmodule Iptools do
+  @doc """
+  Determine if an ip address is an RFC1918 address. Reserved for local
+  communications within a private network as specified by RFC 1918.
+  Returns true if it is an RFC1918 address
+  """
+  @spec is_rfc1918?(String.t) :: boolean()
+  def is_rfc1918?(inString) do
+    case String.split(inString, ".") do
+      ["10",_,_,_] -> true
+      ["192","168",_,_] -> true
+      ["172",x,_,_] -> 16 <= String.to_integer(x) && String.to_integer(x) <= 31
+      _ -> false
+    end
+  end
+
+  @doc """
+  convert an ip address represented as a dotted-decimal string to
+  an integer
+  """
+  @spec to_integer(String.t) :: integer()
+  def to_integer(inString) do
+    # Example input "10.0.0.1"
+
+    String.split(inString, ".") # ["10","0","0","1"]
+    |> Enum.map(fn(x) -> String.to_integer(x) end) # [10,0,0,1]
+    |> Enum.zip([3,2,1,0]) #[{10,3}, {0,2}, {0,1}, {1,0}]
+    |> Enum.reduce(0, fn({value, exponent}, acc) -> acc + (value * :math.pow(256, exponent)) end) # 167772161.0
+    |> round #167772161
+  end
 end
--- test/iptools_test.exs
+++ test/iptools_test.exs
@@ -2,7 +2,21 @@ defmodule IptoolsTest do
   use ExUnit.Case
   doctest Iptools
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "identifies RFC1918 ip addresses" do
+    assert Iptools.is_rfc1918?("10.10.10.10") == true
+    assert Iptools.is_rfc1918?("172.16.10.10") == true
+    assert Iptools.is_rfc1918?("192.168.1.1") == true
+    assert Iptools.is_rfc1918?("172.15.10.10") == false
+    assert Iptools.is_rfc1918?("192.30.252.130") == false
+  end
+
+  test "converts dotted decimal ips to integers" do
+    assert Iptools.to_integer("0.0.0.0") == 0
+    assert Iptools.to_integer("0.10.0.0") == 655360
+    assert Iptools.to_integer("0.0.10.0") == 2560
+    assert Iptools.to_integer("0.0.0.10") == 10
+    assert Iptools.to_integer("10.0.0.0") == 167772160
+    assert Iptools.to_integer("204.14.239.82") == 3423530834
+    assert Iptools.to_integer("255.255.255.255") == 4294967295
   end
 end
